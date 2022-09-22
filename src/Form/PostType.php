9216179cdbb8c135219bbc0c8db1c97d75760afd
Benjamin Zaslavsky <benjamin.zaslavsky@gmail.com>
2022-09-22 15:54:14 +0200

    Add CKEditor and terms page

--- src/Form/PostType.php
+++ src/Form/PostType.php
@@ -3,21 +3,30 @@
 namespace App\Form;
 
 use App\Entity\Post;
+use FOS\CKEditorBundle\Form\Type\CKEditorType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use function Symfony\Component\Translation\t;
 
 class PostType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('title')
-            ->add('text')
+            ->add('title', TextType::class, [
+                'label' => t('Post Title'),
+            ])
+            ->add('text', CKEditorType::class, [
+                'label' => t('Content'),
+            ])
             ->add('publicationDate', DateType::class, [
                 'input' => 'datetime_immutable',
                 'widget' => 'single_text',
+                'label' => t('Publication date')
             ]);
 
         if (array_key_exists('edit', $options) && $options['edit'] === true) {
